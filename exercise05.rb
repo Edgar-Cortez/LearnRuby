02fa6dfdb37bb5e2545a5ce886feecbe4f433747
Edgar Cortez <ea.cortezz@gmail.com>
2020-01-27 21:56:15 -0600

    added previous completed  exercies

---
+++ exercise05.rb
@@ -0,0 +1,18 @@
+name = 'Edgar Cortez'
+age = 30
+height = 73
+weight = 191
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+
+in_to_cm = height * 2.54
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Acutally that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{in_to_cm}, and #{weight} I get #{age + height + weight}."

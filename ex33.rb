1589ab6e48dcd4131e590f408602df103b993ee7
Edgar Cortez <ea.cortezz@gmail.com>
2020-02-18 22:20:37 -0600

    completed loops and branches

---
+++ ex33.rb
@@ -0,0 +1,92 @@
+# Version 1
+# i = 0
+# numbers = []
+
+# while i < 6
+#     puts "At the top i is #{i}"
+#     numbers.push(i)
+
+#     i += 1
+#     puts "Numbers now: #{numbers}"
+#     puts "At the bottom i is #{i}"
+# end
+
+# puts "The numbers: "
+
+# for num in numbers
+#     puts num
+# end
+
+# Version 2
+# i = 0
+# numbers = []
+# counter = 6
+# increment = 1
+
+# while i < counter
+#     puts "At the top i is #{i}"
+#     numbers.push(i)
+
+#     i += increment
+#     puts "Numbers now: #{numbers}"
+#     puts "At the bottom i is #{i}"
+# end
+
+# puts "The numbers: "
+
+# for num in numbers
+#     puts num
+# end
+
+# Version 3
+# def test(cntr, incr)
+#     i = 0
+#     numbers = []
+
+#     while i < cntr
+#         puts "At the top i is #{i}"
+#         numbers.push(i)
+
+#         i += incr
+#         puts "Numbers now: #{numbers}"
+#         puts "At the bottom i is #{i}"
+#     end
+
+#     puts "The numbers: "
+
+#     for num in numbers
+#         puts num
+#     end
+# end
+
+# v3 = test(7, 1)
+
+# Version 4
+# Didn't figure out
+# def test(cntr, incr)
+#     numbers = []
+
+    # while i < cntr
+    #     puts "At the top i is #{i}"
+    #     numbers.push(i)
+
+    #     i += incr
+    #     puts "Numbers now: #{numbers}"
+    #     puts "At the bottom i is #{i}"
+    # end
+#     for count in counter do
+#         puts "At the top i is" + count
+#         numbers.push(count)
+
+#         puts "Numbers now: #{numbers}"
+#         puts "At the bottom i is " + count
+#     end
+
+#     puts "The numbers: "
+
+#     for num in numbers
+#         puts num
+#     end
+# end
+
+# v3 = test(7, 1)
